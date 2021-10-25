21e04e49e0f62327ac2b16406069d56f25a44f52
fidelis452 <fideliswaweru19@gmail.com>
2021-10-25 23:09:19 +0300

    Node.js Post CRUD

--- index.js
+++ index.js
@@ -2,14 +2,20 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const authRoute = require("./routes/auth");
+const usersRoute = require("./routes/users");
 
 dotenv.config();
-
+app.use(express.json());
 mongoose.connect("mongodb+srv://fidelis:fidelis@blog.wyny7.mongodb.net/blog?retryWrites=true&w=majority", {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(console.log("Connected to mongo Db"))
   .catch((err)=>console.log(err));
-app.listen("3200", () =>{
+
+  app.use("/api/auth",authRoute)
+  app.use("/api/users",usersRoute)
+
+app.listen("4000", () =>{
     console.log("background is running");
 });
